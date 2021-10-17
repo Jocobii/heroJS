1e6721c7e2c0ed24f0c49fc562f630576b0fd6a7
Alexander Jocobi <48640883+Jocobii@users.noreply.github.com>
2021-10-17 00:03:25 -0700

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,16 +1,9 @@
 module.exports = {
-    /*database2:{
-        host: 'blzl9q3ap5lybfeslhkl-mysql.services.clever-cloud.com',
-        port: 3306,
-        user: 'uz423q0y4fbzki8l',
-        password: 'zjLsQc5Z2vOAc6vzTdEQ',
-        database: 'blzl9q3ap5lybfeslhkl'
-    }*/
     database:{
-        host: 'localhost',
+        host: 'foo',
         port: 3306,
-        user: 'root',
+        user: 'foo',
         password: '',
-        database: 'fkhero'
+        database: 'foodb'
     }
 };
